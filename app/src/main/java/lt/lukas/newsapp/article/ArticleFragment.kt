6a9d8f3286ab68cdcbf969284f4c2df020c93502
Lukas Juodeika <lksjuodeika@gmail.com>
2020-08-02 14:27:58 +0300

    Createinitial article implementation

--- app/src/main/java/lt/lukas/newsapp/article/ArticleFragment.kt
+++ app/src/main/java/lt/lukas/newsapp/article/ArticleFragment.kt
@@ -1,11 +1,14 @@
 package lt.lukas.newsapp.article
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_article.view.*
 import lt.lukas.newsapp.R
 
 class ArticleFragment : Fragment() {
@@ -17,6 +20,19 @@ class ArticleFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_article, container, false)
+        val view = inflater.inflate(R.layout.fragment_article, container, false)
+        loadArticle(view)
+        return view
+    }
+
+    private fun loadArticle(view: View) {
+        view.textViewTitle.text = args.article.title
+        view.textViewDescription.text = args.article.description
+        view.textViewAuthor.text = args.article.author
+        view.textViewDate.text = args.article.publishedAt
+        view.buttonReadFullArticle.setOnClickListener {
+            val uri = Uri.parse(args.article.url)
+            startActivity(Intent(Intent.ACTION_VIEW, uri))
+        }
     }
 }
