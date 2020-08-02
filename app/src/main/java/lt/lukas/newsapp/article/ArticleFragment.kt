ffe73f6e2a9e87cce7f8ae477cb5e2a7c6a8f165
Lukas Juodeika <lksjuodeika@gmail.com>
2020-08-02 16:50:11 +0300

    Implement collapsing toolbar with image loading

--- app/src/main/java/lt/lukas/newsapp/article/ArticleFragment.kt
+++ app/src/main/java/lt/lukas/newsapp/article/ArticleFragment.kt
@@ -1,6 +1,7 @@
 package lt.lukas.newsapp.article
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,8 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_article.*
 import kotlinx.android.synthetic.main.fragment_article.view.*
 import lt.lukas.newsapp.R
+import lt.lukas.newsapp.entities.Article
 
 class ArticleFragment : Fragment() {
 
@@ -21,17 +25,24 @@ class ArticleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_article, container, false)
-        loadArticle(view)
+        view.collapsingToolbar.setExpandedTitleColor(Color.TRANSPARENT)
+        loadArticle(view, args.article)
         return view
     }
 
-    private fun loadArticle(view: View) {
-        view.textViewTitle.text = args.article.title
-        view.textViewDescription.text = args.article.description
-        view.textViewAuthor.text = args.article.author
-        view.textViewDate.text = args.article.publishedAt
+    override fun onResume() {
+        super.onResume()
+        appBarLayout.setExpanded(false)
+    }
+
+    private fun loadArticle(view: View, article: Article) {
+        view.textViewTitle.text = article.title
+        view.textViewDescription.text = article.description
+        view.textViewAuthor.text = article.author
+        view.textViewDate.text = article.publishedAt
+        Glide.with(this).load(article.urlToImage).into(view.imageView)
         view.buttonReadFullArticle.setOnClickListener {
-            val uri = Uri.parse(args.article.url)
+            val uri = Uri.parse(article.url)
             startActivity(Intent(Intent.ACTION_VIEW, uri))
         }
     }
